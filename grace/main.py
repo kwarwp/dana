8d51e34044552a85d7800c0eb66e1e48d5933d25
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-04-24 15:36:45 -0300

    Saved grace/main.py at Timestamp: 2018-04-24 18:36:44

--- grace/main.py
+++ grace/main.py
@@ -1,6 +1,6 @@
 # dana.grace.main.py
 from _spy.vitollino.main import Cena,Elemento, Texto
-from _spy.vitollino.main import IVENTARIO as inv
+from _spy.vitollino.main import INVENTARIO as inv
 
 ilha = "https://gartic.com.br/imgs/mural/_v/_vorreipace_/ilha.png"
 floresta_heterogenea = "https://img.elo7.com.br/product/original/12F2B86/painel-de-festa-floresta-festa-personalizada.jpg"
@@ -11,16 +11,16 @@ habbo = "https://wydnblog.files.wordpress.com/2010/08/miss.png"
 class criarcenas():
    def __init__(self):
 
-       self.ilha = Cena(img=ilha)
-       self.habbo = Elemento(img=habbo, tit="voce perdido", style = dict( left = 90,
-                                                                          height = 200,
-                                                                          width = 60, 
-                                                                          top = 100))
-       ##self.pergaminho = Elemento(img=pergaminho, = dict ( left = 90,height = 200, width = 60,    top = 100))
+     self.ilha = Cena(img=ilha)
+     self.habbo = Elemento(img=habbo, tit="voce perdido", style = dict( left = 90,
+                                                                        height = 200,
+                                                                        width = 60, 
+                                                                        top = 100))
+     self.pergaminho = Elemento(img=pergaminho, = dict ( left = 90,height = 200, width = 60,    top = 100))
                                                                                       
                                                                                         
                                                                                          
-      self.instrucao = texto(self.pergaminho,"água,fogo,abrigo,comida")  
+     self.instrucao = texto(self.pergaminho,"água,fogo,abrigo,comida")  
       
       
       self.habbo.entra(self.ilha)
@@ -29,6 +29,5 @@ class criarcenas():
       
       self.ilha.vai()
    
-   if __name__ == "__main__":
-   
-   criarcenas()
+if __name__ == "__main__":
+     criarcenas()
