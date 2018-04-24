510d3a4a73668f7e1d587466c670af0c20db6b18
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-04-24 14:44:50 -0300

    Saved sara/main.py at Timestamp: 2018-04-24 17:44:50

--- sara/main.py
+++ sara/main.py
@@ -1,1 +1,7 @@
 # dana.sara.main.py
+from _spy.vitollino.main import Cena, Texto, Elemento
+from _spy.vitollino.main import INVENTARIO as inv
+
+ILHA = "https://gartic.com.br/imgs/mural/_v/_vorreipace_/ilha.png"
+FLORESTA_HETEROGENEA = "https://i.pinimg.com/originals/38/13/36/381336266455e3f9c622b35ff9593a25.jpg"
+FLORESTA_HOMOGENEA = "/home/todomundo/Desktop/Forest-pine-trees-sun-rays_1920x1200.jpg"
