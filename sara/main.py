bc9f0fcf5d8dd1b76c7221e699e5cc5083fcdeba
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-04-26 15:45:52 -0300

    Saved sara/main.py at Timestamp: 2018-04-26 18:45:52

--- sara/main.py
+++ sara/main.py
@@ -1,16 +1,14 @@
 # dana.sara.main.py
 from _spy.vitollino.main import Cena, Elemento, INVENTARIO as inv
 from _spy.vitollino.main import STYLE
-STYLE ["width"] = 900
-STYLE ["min-hight"] = 600
+STYLE ["width"] = 1000
+#STYLE ["min-hight"] = 600
 
 floresta = "https://vignette.wikia.nocookie.net/reinoanimalia/images/c/c3/Bosques.png/revision/latest?cb=20150820071051&path-prefix=es"
 coqueiro = "https://2.bp.blogspot.com/-QutwWEONdV0/WSA8KeAeToI/AAAAAAAAJJM/2vz0mWLvEZY-ZPc_EsWi2Nl9O39WSvUkQCLcB/s1600/coqueiro-an%25C3%25A3o-produzindo.jpg"
-orvalho = "https://st.depositphotos.com/1030956/4910/i/950/depositphotos_49103197-stock-photo-macro-tree-branch-with-raindrops.jpg"
+orvalho = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-_3_HMMVXDWcMI9dIi_BI6i3F5jV-_983RseAwJ-DpEO2RoSP"
 seiva = "http://cdn.ciclovivo.com.br/wp-content/uploads/2015/009/img/noticias/1238633982_9f95150143%20(1).jpg"
 cacto = "https://www.guiamuriae.com.br/wp-content/uploads/2016/01/Agua-de-cacto-Foto-Pixabay.jpg"
-
-
 
 class Agua():
      def __init__ (self):
@@ -36,10 +34,10 @@ class Agua():
       self.florestaSeiva.esquerda = self.florestaCacto
       self.florestaCacto.direita = self.florestaSeiva
      
-      self.minicoqueiro= Elemento(img=coqueiro, style=dict (left=100, top= 300, height=200 ,width=200,bottom=100))
-      self.miniorvalho= Elemento (img=orvalho, style=dict (left=95, top= 100, height=100, width=100, bottom=15))
-      self.miniseiva= Elemento (img=seiva, style=dict (left=100, top= 100, height=100, width=100, bottom=15))
-      self.minicacto= Elemento (img=cacto, style=dict (left=95, top= 100, height=100, width=100, bottom=15))
+      self.minicoqueiro= Elemento(img=coqueiro, style=dict (left=100, top=1000, height=200,width=200,bottom=200))
+      self.miniorvalho= Elemento (img=orvalho, style=dict (left=200, top= 500, height=200, width=200, bottom=200))
+      self.miniseiva= Elemento (img=seiva, style=dict (left=500, top= 300, height=200, width=200, bottom=200))
+      self.minicacto= Elemento (img=cacto, style=dict (left=300, top= 2500, height=200, width=200, bottom=200))
      
       self.minicoqueiro.entra (self.florestaCoqueiro)
       self.minicacto.entra (self.florestaCacto)
