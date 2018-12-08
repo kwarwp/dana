336925522ad0b54e7da483666adee13b66e90d7f
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-12-08 10:55:09 -0200

    Created libby/main.py at Timestamp: 2018-12-08 12:55:09

---
+++ libby/main.py
@@ -0,0 +1,56 @@
+# dana.libby.main.py
+from _spy.vitollino.main import Cena, Elemento, STYLE, Codigo as Code, INVENTARIO, NS
+STYLE["width"] = 800
+class Foto:
+    SPG= "https://imgur.com/TPGqQ7k.png"
+    IN01="https://imgur.com/Lqx1fWA.png"
+    MIL0="https://imgur.com/ojAlOFl.png"
+    PFOR="https://imgur.com/dbFMO8W.png"
+    FORE="https://imgur.com/Ac9gSDD.png"
+    SBCC="https://imgur.com/6vaIIR1.png"
+    SPPT="https://imgur.com/EUjDgk2.png"
+    KWOP="https://imgur.com/yfL0EFR.png"
+    MAN0="https://imgur.com/RJ4dNv9.png"
+    MAN1="https://imgur.com/Vx6zqTS.png"
+    MAN2="https://imgur.com/rYPXyYM.png"
+    SPGG="https://imgur.com/yAAFXJd.png"
+    SPCC="https://imgur.com/6vWvrAW.png"
+    SPGF="https://imgur.com/EjW4X9B.png"
+    SPGE="https://imgur.com/UZHl9z3.png"
+    SPGP="https://imgur.com/WIJiQ0A.png"
+    SPGU="https://imgur.com/KoLkTix.png"
+    SPGC="https://imgur.com/7ezi6zF.png"
+    SGVI="https://youtu.be/LsRdPQ8Stq8"
+    
+class Codigo(Code):
+    def __init__(self, codigo="", topo="", cena=INVENTARIO, img="", vai=None, style=NS):
+        Code.__init__(self, codigo=codigo, topo=topo, cena=cena, img=img, vai=vai, style=style)
+        a = html.A("×", href="#", style=dict(position="absolute", top="0px", right="10px",
+        fontSize="30px", fontWeight="bold"))  
+        a.onclick = self._close
+        self.elt<=a
+        self._close()
+        #self.elt.style = {"visibility": "hidden", "opacity": 0}
+    def _close(self, *_):
+        self.elt.style = {"visibility": "hidden", "opacity": 0}
+        self.cena._code_=self
+
+class Video(Elemento):
+    def __init__(self,source, width, height, top, left):
+        Elemento.__init__(self,style=dict(position="absolute", top=top,left=left))
+        video=html.VIDEO(width=width, height=height,autoplay=True, style=dict(position="absolute", top=top,
+        left=left))
+        video<=html.SOURCE(src=source)
+        video.onclick = self._close
+        self.elt<=video
+
+    def _close(self, *_):
+        self.elt.style = {"visibility": "hidden", "opacity": 0}
+        self.cena._video_=self
+    
+c = Cena(Foto.SPG)
+e = Elemento(Foto.MIL0, style=dict(left=100, top=100, width="100px", height="120px"))
+e.entra(c)
+c.vai()
+v = Video(SGVI, 600, 400, 100, 100)
+v.entra(c)
