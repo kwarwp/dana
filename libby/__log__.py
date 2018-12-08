fced8b17e026aa6fa05550bc3f4f9cee0ac333a6
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-12-08 10:54:48 -0200

    Saved libby/__log__.py at Timestamp: 2018-12-08 12:54:48

--- libby/__log__.py
+++ libby/__log__.py
@@ -78,3 +78,27 @@ Traceback (most recent call last):
     def __init__(self, codigo="", topo="", cena=INVENTARIO, img="", vai=None, style=NS):
 NameError: name 'NS' is not defined
 '''},
+{'date': 'Sat Dec 08 2018 10:54:46.728 GMt-0200 (Horário de Verão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 55
+    v = Video(SGVI, 600, 400, 100, 100)
+NameError: name 'SGVI' is not defined
+'''},
