e5da2a76a71e92442f8336eaeb7dafcae9aa7f05
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2018-12-08 11:05:22 -0200

    Saved libby/__log__.py at Timestamp: 2018-12-08 13:05:21

--- libby/__log__.py
+++ libby/__log__.py
@@ -125,3 +125,19 @@ NameError: name 'SGVI' is not defined
                                                                                      ^
 SyntaxError: invalid syntax
 '''},
+{'date': 'Sat Dec 08 2018 11:05:20.926 GMt-0200 (Horário de Verão de Brasília) -X- SuPyGirls -X-',
+'error': '''Traceback (most recent call last):
+  module _core.main line 160
+    dialog.action(lambda *_: self.start()
+  module _core.supygirls_factory line 135
+    self.act(self, lambda *_: self.hide() or extra()) if self.act else None
+  module _core.supygirls_factory line 306
+    return self._first_response(lambda: self._executa_acao(), self.extra, self.error)
+  module _core.supygirls_factory line 278
+    traceback.print_exc(file=sys.stderr)
+  module _core.supygirls_factory line 295
+    exec(self.code, glob)  # dict(__name__="__main__"))
+  module <module> line 67
+    v = Embed(SGVI, 600, 400, 100, 100)
+NameError: name 'SGVI' is not defined
+'''},
