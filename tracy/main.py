393ec5d71471f04a2fb4e1fcefed0baf9ccf7122
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-01 11:06:15 -0300

    Created tracy/main.py @2019-11-01 14:06

---
+++ tracy/main.py
@@ -0,0 +1,11 @@
+# dana.tracy.main.py
+from _spy.vitollino.main import Cena,Elemento
+from _spy.vitollino.main import INVENTARIO as inv
+MELIODAS="https://p7.hiclipart.com/preview/300/561/296/the-seven-deadly-sins-meliodas-kirito-others.jpg"
+CIDADE="estadodedireito.com.br/wp-content/uploads/2016/04/Cidade.png"
+class naruto():
+    meliodas= Elemento(img=MELIODAS)
+    cidade= cena(img=CIDADE)
+    meliodas.entra(cidade)
+    cidade.vai
+naruto()
