ce2fba7997e36ecbb933212fa5e68f3d242dce85
SuPyGirls Empowering Womam with Python Skills <38007182+kwarwp@users.noreply.github.com>
2019-11-01 11:36:35 -0300

    Saved tracy/main.py @2019-11-01 14:36

--- tracy/main.py
+++ tracy/main.py
@@ -3,4 +3,10 @@ from _spy.vitollino.main import INVENTARIO as inv
 PEAK="https://3.bp.blogspot.com/-5tB1GgLVsLQ/W2hMvezUuzI/AAAAAAAABFQ/cIvbYbCP6TsMove96s3TXh20pxJglTYNgCLcBGAs/s1600/Screenshot_2018-08-06-18-52-14.png"
 SQUAD="https://hdqwalls.com/download/garena-free-fire-z4-2560x1440.jpg"
 class diamante():
-    p 
+    peak= Cena(img=PEAK)
+    squad= Elemento(img=SQUAD)
+    squad.entra(peak)
+    peak.vai()
+    
+diamante()
+    
